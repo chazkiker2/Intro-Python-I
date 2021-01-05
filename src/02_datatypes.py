22007066d2c635675f0d21d9764305202e314978
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-01-04 19:19:50 -0600

    t02-t05

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -15,7 +15,12 @@ y = "7"
 
 # YOUR CODE HERE
 
+# number + string = number
+print(x + int(y))
+
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+# string + number = string
+print(str(x) + y)
--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,40 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
+x.extend(y)
 print(x)
+# x += y
+# print(x)
+# print(x + y)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
+# x.remove(8)
+# print(x)
+# x.pop(-3)
+# x.pop(index) — remove el at index
+print(f"***index of el=8 in x[] — {x.index(8)}")
+# x.index(elToFind) = location of el
+x.pop(x.index(8))
 print(x)
+# print(x[-3])
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+# x.insert(location, element)
+x.insert(-1, 99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for num in x:
+    print(num * 1000)
